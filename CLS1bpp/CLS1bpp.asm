4daab904254d116f31cd4f33db16f03d1a728b5c
George C-E <gce8111@gmail.com>
2024-05-21 22:41:56 -0500

    implements clear screen method using 1bpp mode

---
+++ CLS1bpp/CLS1bpp.asm
@@ -0,0 +1,39 @@
+#include "includes\ti84pce.inc"
+
+.assume ADL=1
+.org userMem-2
+.db tExtTok,tAsm84CeCmp
+
+ call _GetKey
+ 
+ ld hl,Palette
+ ld de,mpLcdPalette
+ ld bc,2*2
+ ldir
+ 
+ ld a,lcdBpp1
+ ld (mpLcdCtrl),a
+
+ ld hl,vRam
+ ld bc,320*240/8		;320 by 240 pixels, 8 pixels per byte, iterate through every byte of mem
+CLSLoop:
+ ld (hl),$00			;set 8 pixels at a time
+ inc hl
+ 
+ dec bc					;using bc to countdown how many iterations we have left
+ ld a,b					;check if zero left
+ or c
+ jp nz,CLSLoop			;repeat if non-zero amount left
+
+ call _GetKey
+ 
+ ld a,lcdBpp16
+ ld (mpLcdCtrl),a
+ 
+ ;call _HomeUp
+ 
+ ret
+ 
+Palette:
+.dw $0000				;black
+.dw $FFFF				;white
